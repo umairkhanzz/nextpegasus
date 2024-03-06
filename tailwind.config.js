4a051aaa01ea1d4ef1859efbd1cf6b61dd6e38fd
Muhammad Umair Khan <129543803+umairkhanzz@users.noreply.github.com>
2024-03-06 20:53:14 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'market-analysis': 'url(../images/in-wave-background-1.png)',
+        
+      },
+     
+    },
+  },
+  plugins: [],
+}
+
